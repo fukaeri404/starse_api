a88b1a5e8861d5bd67558745017cdf3c1565a751
khinezarthwe <khinezar70122107@gmail.com>
2023-10-04 09:35:12 +0630

    login successful v1

--- src/main/java/co/jp/starse/kintai/config/AuthorizeRequest.java
+++ src/main/java/co/jp/starse/kintai/config/AuthorizeRequest.java
@@ -9,7 +9,8 @@ public class AuthorizeRequest {
 	static void configureAuthorizeRequests(
 			AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry request) {
 		request.requestMatchers(HttpMethod.POST, Routes.LOGIN).permitAll()
-				.requestMatchers(Routes.User, Routes.CHANGE_PASSWORD).hasAnyAuthority("1", "2", "3", "4")
+		        .requestMatchers(HttpMethod.POST, Routes.REGISTER).hasAnyAuthority("1","2")
+				.requestMatchers(Routes.User, Routes.CHANGE_PASSWORD).hasAnyAuthority("1", "2", "3")
 				.requestMatchers(Routes.PUBLIC, Routes.FORGOT_PASSWORD).permitAll()
 				.requestMatchers(Routes.RESET_PASSWORD).hasAnyAuthority("1", "2", "3");
 	}
--- src/main/java/co/jp/starse/kintai/config/Routes.java
+++ src/main/java/co/jp/starse/kintai/config/Routes.java
@@ -9,14 +9,13 @@ public class Routes {
 
 	// システム管理者 1 
 	static String Admin = "api/v1/admin/**";
-	static String REGISTER = "api/v1/register";
+	static String REGISTER = "api/v1/admin/register";
 
 	// グループ主任 2 
 	static String Shoninsha = "api/v1/shonin_sha/**";
 
-	// 一般社員３
 
-	// お客様4
+	// 一般社員３
 	static String User = "api/v1/user/**";
 
 	// システム管理者1, 運用管理者２, 一般社員３
